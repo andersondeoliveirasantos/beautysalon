f92ab2666460db6746447e601ed1e23ef6407171
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2021-11-06 16:34:03 -0300

    JavaScript

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* Abre e fecha o Menu quando clicar no icone: hamburguer e x */
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelector('nav .toggle')
 
@@ -5,4 +6,11 @@ for (const element of toggle) {
   element.addEventListener('click', function () {
     nav.classList.toggle('show')
   })
+}
+
+/* Quando clicar em um icone do Menu, esconder o Menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for(const link of links) {
+  nav.classList.remove('show')
 }
