df2f68eb6f1f4736b815d9db69d027b29e5bf27c
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2021-11-06 18:50:59 -0300

    rolagem-suave

--- main.js
+++ main.js
@@ -12,5 +12,21 @@ for (const element of toggle) {
 const links = document.querySelectorAll('nav ul li a')
 
 for(const link of links) {
-  nav.classList.remove('show')
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
 }
+
+/* Mudar o header da página, quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function() {
+  if(window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // Menor que a altura do header
+    header.classList.remover('scroll')
+  }
+})
