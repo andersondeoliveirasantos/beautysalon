fe6b57cdcb731e3e4990b73879fe51804020b29a
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2021-11-06 16:28:52 -0300

    variaveis2

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelector('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
