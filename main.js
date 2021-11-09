f96a6051215e6c6784d6116f97e1b2b42183bfac
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2021-11-09 09:07:47 -0300

    media-queries-contact

--- main.js
+++ main.js
@@ -39,6 +39,12 @@ const swiper = new Swiper('.swiper-container', {
   },
   mousewheel: true,
   keyboard: true
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 /* ScrollReveal: Mostrar elementos quando der scroll na página */
