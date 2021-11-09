14f7f372d203dd4c47ff42d261df18c669455f34
Anderson de Oliveira Santos <anderson.olliver@outlook.com>
2021-11-09 06:16:42 -0300

    media-queries

--- main.js
+++ main.js
@@ -18,10 +18,10 @@ for (const link of links) {
 }
 
 /* Mudar o header da página, quando der scroll */
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeight) {
     // scroll é maior que a altura do header
     header.classList.add('scroll')
@@ -29,7 +29,7 @@ window.addEventListener('scroll', function () {
     // Menor que a altura do header
     header.classList.remove('scroll')
   }
-})
+}
 
 /* Testimonials carousel slider swiper */
 const swiper = new Swiper('.swiper-container', {
@@ -54,7 +54,7 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
   footer .brand, footer .social
   `,
   { interval: 100 }
@@ -72,7 +72,7 @@ function backToTop() {
 }
 
 /* When Scroll */
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
   backToTop()
 })
